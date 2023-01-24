50fd09efd999170aadcad4bd9c959074dc253894
irvan assiakoley <irvan.assiakoley@epitech.eu>
2023-01-24 17:58:15 +0100

    [ADD] exo 2

--- file.py
+++ file.py
@@ -9,3 +9,23 @@ for num in range(1,101):
     else:
         print(num)
 
+
+
+def findSum(str1):
+
+	temp = "0"
+	Sum = 0
+
+	for ch in str1:
+
+		if (ch.isdigit()):
+			temp += ch
+		else:
+			Sum += int(temp)
+			temp = "0"
+
+	return Sum + int(temp)
+
+str1 = ["168", "4"]
+
+print(findSum(str1))
